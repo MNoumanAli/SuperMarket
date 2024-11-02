b606c9cc3c57a50b92e9719c428edff11aaea03f
MNoumanAli <mnoumanali001@gmail.com>
2024-11-03 03:11:41 +0500

    added categories view and edit page

---
+++ SuperMarket/Controllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SuperMarket.Models;
+
+namespace SuperMarket.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Edit([FromRoute]int? id)
+        {
+            Product product = new Product
+            {
+                Name = "Base",
+                Id = id,
+                Description = $"Description of product {id}"
+            };
+            return View(product);
+        }
+    }
+}
