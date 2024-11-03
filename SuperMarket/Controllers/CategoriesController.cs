f91929e67e408f0affc0cd518b72ca6fa77e2e50
MNoumanAli <mnoumanali001@gmail.com>
2024-11-04 03:29:54 +0500

    add edit/add product routes

--- SuperMarket/Controllers/CategoriesController.cs
+++ SuperMarket/Controllers/CategoriesController.cs
@@ -7,17 +7,35 @@ namespace SuperMarket.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var products = CatogriesRepository.getProducts();
+            return View(products);
         }
-
+        public IActionResult Add()
+        {
+            var product = new Product();
+            return View(product);
+        }
+        [HttpPost]
+        public IActionResult Add(Product product)
+        {
+            if(CatogriesRepository.isProductEmpty(product))
+            {
+                ViewBag.error = true;
+                return View(product);
+            }
+            CatogriesRepository.addProducts(product);
+            return View("Index", CatogriesRepository.getProducts());
+        }
         public IActionResult Edit([FromRoute]int? id)
         {
-            Product product = new Product
-            {
-                Name = "Base",
-                Id = id,
-                Description = $"Description of product {id}"
-            };
+            var product = CatogriesRepository.getProductById(id.HasValue? id.Value : 0);
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            CatogriesRepository.updateProduct(product.Id.HasValue ? product.Id.Value : 0, product);
             return View(product);
         }
     }
